8a94a49f0080fd3ec17a17f14e2fd79c01027a55
Zeeshan Hooda <zeeshanhooda@icloud.com>
2021-01-10 21:11:49 -0700

    added: initial token base

---
+++ token/token.go
@@ -0,0 +1,36 @@
+package token
+
+// TokenType is the type of token
+type TokenType string
+
+// Token is a language token
+type Token struct {
+	Type    TokenType
+	Literal string
+}
+
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+
+	// Identifiers & literals
+	IDENT = "IDENT" // add, foobar, x, y, ...
+	INT   = "INT"   //18636463
+
+	// Operators
+	ASSIGN = "="
+	PLUS   = "+"
+
+	// Delimiters
+	COMMA     = ","
+	SEMICOLON = ";"
+
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+	// Keywords
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+)
