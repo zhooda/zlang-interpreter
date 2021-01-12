73a6c6e70dfab5a43679c3b3528f8b1a3d8b22a9
Zeeshan Hooda <zeeshanhooda@icloud.com>
2021-01-11 20:51:03 -0700

    added: repl base which outputs lexed tokens

---
+++ repl/repl.go
@@ -0,0 +1,32 @@
+package repl
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"zlang/lexer"
+	"zlang/token"
+)
+
+// PROMPT is the repl prompt
+const PROMPT = "z> "
+
+// Start will start the repl
+func Start(in io.Reader, out io.Writer) {
+	scanner := bufio.NewScanner(in)
+
+	for {
+		fmt.Fprintf(out, PROMPT)
+		scanned := scanner.Scan()
+		if !scanned {
+			return
+		}
+
+		line := scanner.Text()
+		l := lexer.New(line)
+
+		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
+			fmt.Fprintf(out, "%+v\n", tok)
+		}
+	}
+}
