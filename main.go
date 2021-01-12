c83655e259d3bdb81c35810c63208861cbe8586f
Zeeshan Hooda <zeeshanhooda@icloud.com>
2021-01-11 21:05:01 -0700

    added: repl to main

--- main.go
+++ main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/user"
+	"zlang/repl"
+)
 
 func main() {
-	fmt.Println("zlang-interpreter")
+	user, err := user.Current()
+	if err != nil {
+		log.Panic(err)
+	}
+	fmt.Printf("hello %s, welcome to z\n", user.Username)
+	fmt.Printf("type some commands\n")
+	repl.Start(os.Stdin, os.Stdout)
 }
